bfd98bec4e3babf90c7b97e50fd5c71f735f1151
_ Trentini _ <pietrotrentini5@gmail.com>
2021-02-16 15:04:07 +0100

    IsDetermined

--- EquazioniLibrary.Test/EquazioniTest.cs
+++ EquazioniLibrary.Test/EquazioniTest.cs
@@ -1,13 +1,25 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
+using EquazioniLibrary;
 namespace EquazioniLibrary.Test
 {
     [TestClass]
     public class EquazioniTest
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestIsDeterminedTrue()
         {
+            double a = 4;
+            bool valore_aspettato = true;
+            bool prova = Equazioni.IsDetermined(a);
+            Assert.AreEqual(valore_aspettato, prova);
+        }
+        [TestMethod]
+        public void TestIsDeterminedFalse()
+        {
+            double a = 0;
+            bool valore_aspettato = false;
+            bool prova = Equazioni.IsDetermined(a);
+            Assert.AreEqual(valore_aspettato, prova);
         }
     }
 }
--- EquazioniLibrary/Equazioni.cs
+++ EquazioniLibrary/Equazioni.cs
@@ -4,5 +4,12 @@ namespace EquazioniLibrary
 {
     public class Equazioni
     {
+        public static bool IsDetermined(double a)
+        {
+            if (a != 0)
+                return true;
+            return false;
+
+        }
     }
 }
