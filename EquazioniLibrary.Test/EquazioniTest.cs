5a582717136fdd28732a212e373118b44fe387a2
_ Trentini _ <pietrotrentini5@gmail.com>
2021-02-16 15:00:28 +0100

    Add project files.

---
+++ EquazioniLibrary.Test/EquazioniTest.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EquazioniLibrary.Test
+{
+    [TestClass]
+    public class EquazioniTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
